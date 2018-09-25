6e794f68312d6fd40be834fcb6d80afde9be3380
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2018-09-25 13:16:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,19 @@ int main(){
     std::cout << "What would you like to calculate today? Enter 'f' for Fibonacci functions, 'g' for Greatest Common Divisors, and 'l' for Lowest Common Multiple. " << std::endl;
 
     std::cin >> user_input;
+    //As prompted by user, finds GCD.
     if(user_input == 'g' || user_input == 'G'){
         gcd();
         std::cout << std::endl;
     }
+    
+    //As prompted by user, finds Fibonacci sequence or term.
     else if(user_input == 'f'|| user_input == 'F'){
         fibonacci();
         std::cout << std::endl;
     }
+    
+    //As prompted by user, finds LCM.
     else if(user_input == 'l' || user_input == 'L'){
         lcm();
         std::cout << std::endl;
