a7fda25b828d37423886c4ae5de1e328ec1d64c0
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2018-09-29 22:17:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include "mathfuncs.h"
+#include "functions_set.h"
 
 int main(){
+    //Sequences and special functions
+    std::cout << "These are for sequences and special functions." << std::endl;
+    long int input1{0};
+    std::cin >> input1;
+
+    std::cout << "Collatz" << std::endl;
+    collatz(input1);
+    std::cout << std::endl;
+    std::cout << "Fibonacci sequence" << std::endl;
+    fib_seq(input1);
+    std::cout << std::endl;
+    std::cout << "Fibonacci term" << std::endl;
+    fib_term(input1);
+    std::cout << std::endl;
+    std::cout << "Reversing your number and testing if it's a palindrome." << std::endl;
+    palindrome(input1);
+    std::cout << std::endl << std::endl;
+
+    //GCD and LCM
+    std::cout << "These are for GCD and LCM." << std::endl;
+    std::cin >> input1;
+    long int input2{0};
+    std::cin >> input2;
+
+    std::cout << "GCD" << std::endl;
+    gcd(input1, input2);
+    std::cout << std::endl;
+    std::cout << "LCM" << std::endl;
+    lcm(input1, input2);
+    std::cout << std::endl << std::endl;
+
+    //If it's prime
+    std::cout << "These are for testing if your number is prime." << std::endl;
+    unsigned long int input3{0};
+    std::cin >> input3;
+
+    prime(input3);
+    std::cout << std::endl << std::endl;
     
+    //Primes between two numbers
+    std::cout << "These are for finding primes within a range (inclusive)." << std::endl;
+    std::cin >> input1;
+    std::cin >> input2;
+
+    primerange(input1, input2);
+
     return(0);
 }
