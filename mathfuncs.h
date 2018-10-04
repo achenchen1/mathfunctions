3a90a9f81f964be0375b853000347af7e713bf63
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2018-10-03 20:06:50 -0400

    Update mathfuncs.h

--- mathfuncs.h
+++ mathfuncs.h
@@ -6,34 +6,40 @@
 //Pertains only to functions with sequences
 class Sequences {
 public:
-    void fib_seq(long int terminalnumber);
-    long unsigned int fib_term(long int terminalnumber);
-    void collatz(long int terminalnumber);
+    long int terminalnumber;
+
+    void fib_seq();
+    long unsigned int fib_term();
+    void collatz();
 };
 
 //Pertains to miscellaneous functions
 class Misc {
 public:
-    void friendly(long unsigned int user_input);
-    int friendly_counter(long unsigned int user_input);
-    long int reverse(long int user_input);
-    bool palindrome(long int user_input);
+    long unsigned int user_input;
+
+    void friendly();
+    int friendly_counter();
+    long int reverse();
+    bool palindrome();
 };
 
 //Pertains to numbers
 class Nums {
 public:
-    long int gcd(long int num1, long int num2);
-    long int lcm(long int num1, long int num2);
-    
-    //Means 
-    long double amean(unsigned int quantnum);
-    long double gmean(unsigned int quantnum);
-    long double hmean(unsigned int quantnum);
-    
+    long unsigned int primary_input;
+    long unsigned int secondary_input;
+
+    long unsigned int gcd();
+    long unsigned int lcm();
+
+    //Means
+    long double amean();
+    long double gmean();
+    long double hmean();
+
     //Prime
-    bool prime(long unsigned int prime1);
-    void primerange(long int prime1, long int prime2);
-    bool primebool(long int prime1);
+    void primerange();
+    bool primebool();
 };
 #endif
